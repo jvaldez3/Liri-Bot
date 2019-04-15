b86b733e59c21af4c4e1d4c685c26da37129984b
jval <joshvaldez3320@gmail.com>
2019-04-15 11:32:54 -0600

    working

---
+++ liri.js
@@ -0,0 +1,118 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+var omdb = (keys.omdb);
+var bandsintown = (keys.bandsintown);
+var request = require('request');
+var fs = require("fs");
+var moment = require('moment');
+
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+
+var userInput = process.argv[2];
+var userQuery = process.argv.slice(3).join(" ");
+
+function userCommand(userInput, userQuery) {
+    switch (userInput) {
+        case "concert-this":
+            concertThis();
+            break;
+        case "spotify-this":
+            spotifyThisSong();
+            break;
+        case "movie-this":
+            movieThis();
+            break;
+        case "do-this":
+            doThis(userQuery);
+            break;
+        default:
+            console.log("I don't understand");
+            break;
+    }
+}
+
+userCommand(userInput, userQuery);
+
+function movieThis() {
+    console.log(`\nSEARCHING FOR..."${userQuery}"`);
+    if (!userQuery) {
+        userQuery = "mean girls";
+    };
+    request("http://www.omdbapi.com/?t=" + userQuery + "&apikey=trilogy", function (error, response, body) {
+        var userMovie = JSON.parse(body);
+
+        var ratingsArr = userMovie.Ratings;
+        if (ratingsArr.length > 2) {}
+
+        if (!error && response.statusCode === 200) {
+            console.log(`\nTitle: ${userMovie.Title}\nCast: ${userMovie.Actors}\nReleased: ${userMovie.Year}\nIMDb Rating: ${userMovie.imdbRating}\nRotten Tomatoes Rating: ${userMovie.Ratings[1].Value}\nCountry: ${userMovie.Country}\nLanguage: ${userMovie.Language}\nPlot: ${userMovie.Plot}\n`)
+        } else {
+            return console.log("Movie able to be found. Error:" + error)
+        };
+    })
+};
+
+function spotifyThisSong() {
+    console.log(`\nSEARCHING FOR..."${userQuery}"`);
+
+    if (!userQuery) {
+        userQuery = "the sign ace of base"
+    };
+    spotify.search({
+        type: 'track',
+        query: userQuery,
+        limit: 1
+    }, function (error, data) {
+        if (error) {
+            return console.log('Error occurred: ' + error);
+        }
+
+        var spotifyArr = data.tracks.items;
+
+        for (i = 0; i < spotifyArr.length; i++) {
+            console.log(`\nArtist: ${data.tracks.items[i].album.artists[0].name} \nSong: ${data.tracks.items[i].name}\nAlbum: ${data.tracks.items[i].album.name}\nSpotify link: ${data.tracks.items[i].external_urls.spotify}\n`)
+        };
+    });
+}
+
+function concertThis() {
+    console.log(`\nSEARCHING FOR...${userQuery}'s next show...`);
+
+    request("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=" + bandsintown, function (error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+
+            var userBand = JSON.parse(body);
+
+            if (userBand.length > 0) {
+                for (i = 0; i < 1; i++) {
+
+                    console.log(`\nArtist: ${userBand[i].lineup[0]} \nVenue: ${userBand[i].venue.name}\nVenue Location: ${userBand[i].venue.latitude},${userBand[i].venue.longitude}\nVenue City: ${userBand[i].venue.city}, ${userBand[i].venue.country}`)
+
+                    var concertDate = moment(userBand[i].datetime).format("MM/DD/YYYY hh:00 A");
+                    console.log(`Date and Time: ${concertDate}\n`);
+                };
+            } else {
+                console.log('Band or concert not found!');
+            };
+        };
+    });
+};
+
+function doThis() {
+
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+
+        var dataArr = data.split(",");
+
+        userInput = dataArr[0];
+        userQuery = dataArr[1];
+
+        userCommand(userInput, userQuery);
+    });
+};
